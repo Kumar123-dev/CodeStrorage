744414a4e4d950b454671e773c9968e74c1174f0
kumar_dev <71014740+Kumar123-dev@users.noreply.github.com>
2020-09-09 18:00:24 +0530

    ExploreTechnologies

---
+++ mydata.java
@@ -0,0 +1,16 @@
+hello
+this is my block
+and lets play with coding 
+using some frmaworks in java
+like 
+springBoot
+springMvc
+spring
+hibernate
+java8
+maven
+junit
+acutators
+git
+Swagger-ui
+postmen
